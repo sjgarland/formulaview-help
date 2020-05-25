36d8ea5011829f7164986b9282dfce4a85c9173f
Stephen Garland <garland@Stephens-MacBook-Pro.local>
2020-05-25 18:34:46 -0400

    Improve style for Help menu in header

--- Website/site.js
+++ Website/site.js
@@ -5,7 +5,7 @@ function toggleMenu() {
 
 /* Close the dropdown menu if the user clicks outside of it */
 window.onclick = function(event) {
-    if (!event.target.matches('.dropdown-label')) {
+    if (!event.target.matches('.dropdown-label') && !event.target.matches('.strong-dropdown-label')) {
         var dropdowns = document.getElementsByClassName("dropdown-content");
         var i;
         for (i = 0; i < dropdowns.length; i++) {
